0f747c19ab6f1755c095dd969de0afc49d410c29
Felonius Gru <new-comet-dev@outlook.com>
2018-05-16 00:16:40 +0400

    correct php code style

--- auto_run.php
+++ auto_run.php
@@ -3,26 +3,30 @@ ini_set('max_execution_time', 600);
 require_once('service_database.php');
 //  $base_url = "https://ats.fuse5live.com/f5api/";
 //  $apikey = "RfiqLWuptjQ5VdkNJpgKBJETAl9YHqqA";
-$mins = (int) date("i");
-$hours = (int) date("H");
+$mins = (int)date("i");
+$hours = (int)date("H");
 
 // it'll be called once, as only once in day hours and mins are 0
-if($hours == 0 && $mins == 0){
+if ($hours == 0 && $mins == 0) {
 	echo "Update Location";
 	updateLocations();
 }
 //It'll be called once an hour as once in hour minutes become 0
-if($mins == 0){
+if ($mins == 0) {
 	echo "Updating Accounts";
 	updateAccounts();
 }
 updateDeliveryLists();
 
-function updateAccounts(){
+function updateAccounts()
+{
 	$offset = getOffset("customer");
-	if($offset == -1){
+	if ($offset == -1) {
 		$offset = getOffset("customer");
-		if($offset == -1){echo "DB connection not available."; return false;}
+		if ($offset == -1) {
+			echo "DB connection not available.";
+			return false;
+		}
 	}
 	$postdata = array();
 	$postdata['data'] = '{
@@ -33,7 +37,7 @@ function updateAccounts(){
 	        "call":"account/all",
 	        "params": null,
 	         "identifier":{
-	              "offset":"'.$offset.'",
+	              "offset":"' . $offset . '",
 	               "limit":"10000",
 	               "sort_column":"account_id",
 	               "sort_order":"asc"
@@ -41,39 +45,41 @@ function updateAccounts(){
 	   }]
 	}';
 	$result = do_curl($postdata);
-	if($result){
-		$result = json_decode($result , 1);
+	if ($result) {
+		$result = json_decode($result, 1);
 		$result = $result["services"][0]["response"];
 		$count = 0;
 		$insertQuery = "INSERT INTO `customer` (`id`, `account_id`, `account_name`, `account_number`, `primary_email`, `default_location`) VALUES ";
-		if($result["status"]){
-			foreach($result["data"]["rows"] as $row){
+		if ($result["status"]) {
+			foreach ($result["data"]["rows"] as $row) {
 				$count++;
-				$insertQuery .= "(NULL, '".$row['account_id']."', '".$row['account_name']."', '".$row['account_number']."', '".$row['primary_email']."', '".$row['default_location']."') ,";
+				$insertQuery .= "(NULL, '" . $row['account_id'] . "', '" . $row['account_name'] . "', '" . $row['account_number'] . "', '" . $row['primary_email'] . "', '" . $row['default_location'] . "') ,";
 			}
-			if($count > 0){
+			if ($count > 0) {
 				$insertQuery = substr($insertQuery, 0, -2);
 				$result = multiQueryDatabase($insertQuery);
-					var_dump($result);
-				if($result){
+				var_dump($result);
+				if ($result) {
 					echo "Updation successful";
-				}
-				else{
+				} else {
 					echo "Error occured";
 				}
-			}	
+			}
 		}
-	}
-	else{
+	} else {
 		echo "Something went wrong";
 	}
 }
 
-function updateLocations(){
+function updateLocations()
+{
 	$offset = getOffset("locations");
-	if($offset == -1){
+	if ($offset == -1) {
 		$offset = getOffset("locations");
-		if($offset == -1){echo "DB connection not available."; return false;}
+		if ($offset == -1) {
+			echo "DB connection not available.";
+			return false;
+		}
 	}
 	$postdata = array();
 	$postdata['data'] = '{
@@ -87,39 +93,41 @@ function updateLocations(){
 		]
 	}';
 	$result = do_curl($postdata);
-	if($result){
-		$result = json_decode($result , 1);
+	if ($result) {
+		$result = json_decode($result, 1);
 		$result = $result["services"][0]["response"];
 		$count = 0;
 		$insertQuery = "INSERT IGNORE INTO `locations`(`id`, `location_name`) VALUES ";
-		if($result["status"]){
-			foreach($result["data"] as $row){
+		if ($result["status"]) {
+			foreach ($result["data"] as $row) {
 				$count++;
-				$insertQuery .= "( '".$row['id']."', '".$row['location_name']."' ) ,";
+				$insertQuery .= "( '" . $row['id'] . "', '" . $row['location_name'] . "' ) ,";
 			}
-			if($count > 0){
+			if ($count > 0) {
 				$insertQuery = substr($insertQuery, 0, -2);
 				$result = multiQueryDatabase($insertQuery);
-					var_dump($result);
-				if($result){
+				var_dump($result);
+				if ($result) {
 					echo "Updation successful";
-				}
-				else{
+				} else {
 					echo "Error occured";
 				}
-			}	
+			}
 		}
-	}
-	else{
+	} else {
 		echo "Something went wrong";
 	}
 }
 
-function updateDeliveryLists(){
+function updateDeliveryLists()
+{
 	$offset = getOffset("delivery_list");
-	if($offset == -1){
+	if ($offset == -1) {
 		$offset = getOffset("delivery_list");
-		if($offset == -1){echo "DB connection not available."; return false;}
+		if ($offset == -1) {
+			echo "DB connection not available.";
+			return false;
+		}
 	}
 	$postdata = array();
 	$postdata['data'] = '{
@@ -130,7 +138,7 @@ function updateDeliveryLists(){
 		{
 			"call": "delivery/all",
 			"identifier": {
-				"offset": '.$offset.',
+				"offset": ' . $offset . ',
 				"limit": 10000,
 				"delivery_log_for": "salesorder",
 				"delivery_log_type": "assigned",
@@ -141,38 +149,40 @@ function updateDeliveryLists(){
 	]
 }';
 	$result = do_curl($postdata);
-	if($result){
-		$result = json_decode($result , 1);
+	if ($result) {
+		$result = json_decode($result, 1);
 		$result = $result["services"][0]["response"];
 		$count = 0;
 		$insertQuery = "INSERT IGNORE INTO `delivery_list` ( `salesorder_delivery_id`, `target_delivery_time`, `driver_run`, `day_run`, `stop_on_run`, `salesorder_location`, `salesorder_number`, `vendor_name`, `line_code`, `product_number`, `counter_person`, `account_name`, `ship_street`, `ship_pobox`, `ship_city`, `ship_state`, `ship_county`, `ship_country`, `ship_code`, `deliveries_assigned_to`) VALUES ";
-		if($result["status"]){
-			foreach($result["data"]["rows"] as $row){
+		if ($result["status"]) {
+			foreach ($result["data"]["rows"] as $row) {
 				$count++;
-				$insertQuery .= "( '".$row['salesorder_delivery_id']."', '".$row['target_delivery_time']."', '".$row['driver_run']."', '".$row['day_run']."', '".$row['stop_on_run']."', '".$row['salesorder_location']."', '".$row['salesorder_number']."', '".$row['vendor_name']."', '".$row['line_code']."', '".$row['product_number']."', '".$row['counter_person']."', '".$row['account_name']."', '".$row['ship_street']."', '".$row['ship_pobox']."', '".$row['ship_city']."', '".$row['ship_state']."', '".$row['ship_county']."', '".$row['ship_code']."', '".$row['deliveries_assigned_to']."') ,";
+				$insertQuery .= "( '" . $row['salesorder_delivery_id'] . "', '" . $row['target_delivery_time'] . "', '" . $row['driver_run'] . "', '" . $row['day_run'] . "', '" . $row['stop_on_run'] . "', '" . $row['salesorder_location'] . "', '" . $row['salesorder_number'] . "', '" . $row['vendor_name'] . "', '" . $row['line_code'] . "', '" . $row['product_number'] . "', '" . $row['counter_person'] . "', '" . $row['account_name'] . "', '" . $row['ship_street'] . "', '" . $row['ship_pobox'] . "', '" . $row['ship_city'] . "', '" . $row['ship_state'] . "', '" . $row['ship_county'] . "', '" . $row['ship_code'] . "', '" . $row['deliveries_assigned_to'] . "') ,";
 			}
-			if($count > 0){
+			if ($count > 0) {
 				$insertQuery = substr($insertQuery, 0, -2);
 				$result = multiQueryDatabase($insertQuery);
-				if($result){
+				if ($result) {
 					echo "Updation successful";
-				}
-				else{
+				} else {
 					echo "Error occured";
 				}
 			}
 		}
-	}
-	else{
+	} else {
 		echo "Something went wrong";
 	}
 }
 
-function updateSalesOrders(){
+function updateSalesOrders()
+{
 	$offset = getOffset("sales_orders");
-	if($offset == -1){
+	if ($offset == -1) {
 		$offset = getOffset("sales_orders");
-		if($offset == -1){echo "DB connection not available."; return false;}
+		if ($offset == -1) {
+			echo "DB connection not available.";
+			return false;
+		}
 	}
 	$postdata = array();
 	$postdata['data'] = '{
@@ -193,75 +203,73 @@ function updateSalesOrders(){
 ]
 }';
 	$result = do_curl($postdata);
-	if($result){
-		$result = json_decode($result , 1);
+	if ($result) {
+		$result = json_decode($result, 1);
 		$result = $result["services"][0]["response"];
 		$count = 0;
 		$insertQuery = "INSERT INTO `sales_orders` (`id`, `sales_order_number`, `sales_order_location`, `sales_order_invoice_number`, `sales_order_total_products`, `sales_order_created_date`, `sales_order_invoice_date`, `sales_order_shipped_via`, `sales_order_grand_total`, `sales_order_id`, `account_balance_terms`, `sales_order_status`) VALUES ";
-		if($result["status"]){
-			foreach($result["data"] as $row){
+		if ($result["status"]) {
+			foreach ($result["data"] as $row) {
 				echo $row['sales_order_number'] . "<br><br>";
 				$count++;
 				continue;
-				$insertQuery .= "(NULL, '".$row['sales_order_number']."', '".$row['sales_order_location']."', '".$row['sales_order_invoice_number']."', '".$row['sales_order_total_products']."', '".$row['sales_order_created_date']."', '".$row['sales_order_invoice_date']."', '".$row['sales_order_shipped_via']."', '".$row['sales_order_grand_total']."', '".$row['sales_order_id']."', '".$row['account_balance_terms']."', '".$row['sales_order_status']."' ) ,";
+				$insertQuery .= "(NULL, '" . $row['sales_order_number'] . "', '" . $row['sales_order_location'] . "', '" . $row['sales_order_invoice_number'] . "', '" . $row['sales_order_total_products'] . "', '" . $row['sales_order_created_date'] . "', '" . $row['sales_order_invoice_date'] . "', '" . $row['sales_order_shipped_via'] . "', '" . $row['sales_order_grand_total'] . "', '" . $row['sales_order_id'] . "', '" . $row['account_balance_terms'] . "', '" . $row['sales_order_status'] . "' ) ,";
 			}
 			echo $count;
 			return;
-			if($count > 0){
+			if ($count > 0) {
 				$insertQuery = substr($insertQuery, 0, -2);
 				$result = multiQueryDatabase($insertQuery);
-				if($result){
+				if ($result) {
 					echo "Updation successful";
-				}
-				else{
+				} else {
 					echo "Error occured";
 				}
 			}
 		}
-	}
-	else{
+	} else {
 		echo "Something went wrong";
 	}
 }
 
 
-function do_curl( array $post = NULL, array $options = array()) {
+function do_curl(array $post = null, array $options = array())
+{
 	$url = "https://ats.fuse5live.com/f5api/";
-    $field_string = http_build_query($post);
-    $defaults = array( 
-        CURLOPT_POST => 1, 
-        CURLOPT_POSTFIELDS => $field_string,
-        CURLOPT_HEADER => 0, 
-        CURLOPT_URL => $url, 
-        CURLOPT_FRESH_CONNECT => 1, 
-        CURLOPT_RETURNTRANSFER => 1, 
-        CURLOPT_FORBID_REUSE => 1, 
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_SSL_VERIFYHOST => 0,
-        CURLOPT_SSL_VERIFYPEER => 0
-        
-    );
+	$field_string = http_build_query($post);
+	$defaults = array(
+		CURLOPT_POST => 1,
+		CURLOPT_POSTFIELDS => $field_string,
+		CURLOPT_HEADER => 0,
+		CURLOPT_URL => $url,
+		CURLOPT_FRESH_CONNECT => 1,
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_FORBID_REUSE => 1,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_SSL_VERIFYHOST => 0,
+		CURLOPT_SSL_VERIFYPEER => 0
 
-    $ch = curl_init(); 
-    curl_setopt_array($ch, ($options + $defaults)); 
-    if( ! $result = curl_exec($ch)) 
-    { 
+	);
+
+	$ch = curl_init();
+	curl_setopt_array($ch, ($options + $defaults));
+	if (!$result = curl_exec($ch)) { 
         //trigger_error(curl_error($ch)); 
-    }
-    if(!$result) {
-	    curl_close($ch);
-		return NULL;
 	}
-    curl_close($ch); 
-    return $result; 
+	if (!$result) {
+		curl_close($ch);
+		return null;
+	}
+	curl_close($ch);
+	return $result;
 }
-function getOffset($table){
+function getOffset($table)
+{
 	$query = "SELECT COUNT(*) FROM `$table`";
 	$result = queryDatabase($query);
-	if($result && $data = mysqli_fetch_array($result)){
+	if ($result && $data = mysqli_fetch_array($result)) {
 		return (int)$data[0];
-	}
-	else{
+	} else {
 		return -1;
 	}
 }
--- service_database.php
+++ service_database.php
@@ -1,35 +1,40 @@
 <?php
+
 function queryDatabase($query)
-{	
-    $conn = mysqli_connect( '35.199.45.70' , "apiuser" , "bignorman56!" , "PTmiddleware" );
-    $result =  mysqli_query($conn , $query);
+{
+    $conn = mysqli_connect('35.199.45.70', "apiuser", "bignorman56!", "PTmiddleware");
+    $result = mysqli_query($conn, $query);
     mysqli_close($conn);
-    return($result);
+    return ($result);
 }
+
 function insertInDatabaseAndGetInsertId($query)
 {
-    $conn = mysqli_connect( '35.199.45.70' , "apiuser" , "bignorman56!" , "PTmiddleware" );
-    $result =  mysqli_query($conn , $query);
-    if($result){
-		$insertId = mysqli_insert_id($conn); 
-	}
+    $conn = mysqli_connect('35.199.45.70', "apiuser", "bignorman56!", "PTmiddleware");
+    $result = mysqli_query($conn, $query);
+    if ($result) {
+        $insertId = mysqli_insert_id($conn);
+    }
     mysqli_close($conn);
-    return($insertId);
+    return ($insertId);
 }
+
 function multiQueryDatabase($query)
 {
-	$insertId = 0;
-    $conn = mysqli_connect( '35.199.45.70' , "apiuser" , "bignorman56!" , "PTmiddleware" );
-    $result =  mysqli_multi_query($conn , $query);
+    $insertId = 0;
+    $conn = mysqli_connect('35.199.45.70', "apiuser", "bignorman56!", "PTmiddleware");
+    $result = mysqli_multi_query($conn, $query);
     mysqli_close($conn);
-    return($result);
+    return ($result);
 }
+
 //Generates Secure String to avoid SQL Injection
 function getSecureString($sourceString)
 {
-    $conn = mysqli_connect( '35.199.45.70' , "apiuser" , "bignorman56!" , "PTmiddleware" );
-	$result =  mysqli_real_escape_string($conn,$sourceString);
+    $conn = mysqli_connect('35.199.45.70', "apiuser", "bignorman56!", "PTmiddleware");
+    $result = mysqli_real_escape_string($conn, $sourceString);
     mysqli_close($conn);
-	return($result);
+    return ($result);
 }
+
 ?>
